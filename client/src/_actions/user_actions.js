a2b870f03d6ab18fd102e432d0aebc48a6ccebb6
Blaise NDIZEYE <68238750+blaise-ndizeye@users.noreply.github.com>
2021-02-26 15:09:06 +0200

    async on user actions
    improve the source code from promises to async await i think it may look better

--- client/src/_actions/user_actions.js
+++ client/src/_actions/user_actions.js
@@ -11,54 +11,49 @@ import {
 } from './types';
 import { USER_SERVER } from '../components/Config.js';
 
-export const registerUser = (dataToSubmit) => {
-    const request = axios.post(`${USER_SERVER}/register`, dataToSubmit)
-        .then(response => response.data);
+export const registerUser = async (dataToSubmit) => {
+    const request = await axios.post(`${USER_SERVER}/register`, dataToSubmit)
 
     return {
         type: REGISTER_USER,
-        payload: request
+        payload: request.data
     }
 }
 
-export const loginUser = (dataToSubmit) => {
-    const request = axios.post(`${USER_SERVER}/login`, dataToSubmit)
-        .then(response => response.data);
+export const loginUser = async (dataToSubmit) => {
+    const request = await axios.post(`${USER_SERVER}/login`, dataToSubmit)
 
     return {
         type: LOGIN_USER,
-        payload: request
+        payload: request.data
     }
 }
 
-export const auth = () => {
-    const request = axios.get(`${USER_SERVER}/auth`)
-        .then(response => response.data);
+export const auth = async () => {
+    const request = await axios.get(`${USER_SERVER}/auth`)
 
     return {
         type: AUTH_USER,
-        payload: request
+        payload: request.data
     }
 }
 
-export const logoutUser = () => {
-    const request = axios.get(`${USER_SERVER}/logout`)
-        .then(response => response.data);
+export const logoutUser = async () => {
+    const request = await axios.get(`${USER_SERVER}/logout`)
 
     return {
         type: LOGOUT_USER,
-        payload: request
+        payload: request.data
     }
 }
 
 
-export const addToCart => (_id) => {
-    const request = axios.get(`${USER_SERVER}/addToCart?productId=${_id}`)
-        .then(response => response.data);
+export const addToCart = async (_id) => {
+    const request = await axios.get(`${USER_SERVER}/addToCart?productId=${_id}`)
 
     return {
         type: ADD_TO_CART_USER,
-        payload: request
+        payload: request.data
     }
 }
 
@@ -113,14 +108,11 @@ export const removeCartItem = (id) => {
 }
 
 
-export const onSuccessBuy = (data) => {
-
-    const request = axios.post(`${USER_SERVER}/successBuy`, data)
-        .then(response => response.data);
-
+export const onSuccessBuy = async (data) => {
+    const request = await axios.post(`${USER_SERVER}/successBuy`, data)
     return {
         type: ON_SUCCESS_BUY_USER,
-        payload: request
+        payload: request.data
     }
 }
 
